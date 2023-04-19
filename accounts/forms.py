26b0d04bf19af58146f79e2f24d243e1dd3c1f6d
William.Mo <billmo0120@gmail.com>
2023-04-19 15:44:08 -0400

    Feature 7 Complete

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+# from receipts.models import Receipt
+# from django.forms import ModelForm
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150, widget=forms.PasswordInput)
